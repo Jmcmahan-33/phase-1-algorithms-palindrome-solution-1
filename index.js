58b0bb8a8fdf6c6ec52a9faa7f255dcfaaaeb956
Jared McMahan <jaredm788@gmail.com>
2022-09-17 10:48:06 -0600

    commit tree a5d68eb7b809fb8f6d363e12c131277cd9c55ece

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+function reverse(word) {
+  const wordArray = word.split("")
+  const reversedWordArray = wordArray.reverse()
+  const reversedWord = reversedWordArray.join("")
+  return reversedWord
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input sting
+  const reversedWord = reverse(word)
+  // if the input is the same as the reversed input 
+ return word === reversedWord
 }
 
 /* 
-  Add your pseudocode here
+  write a function that will return true if a word is a palindrome and false if not. that means if the word is the same as it is backwords, return true. 
+  otherwise flase if not a palindrome.
+
+  reverse the sting 
+
+  if the input is the same as the reversed input 
+  return true
+  else 
+  return false
 */
 
 /*
@@ -12,7 +30,8 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  // expecting mom to === mom //true
+  // expecting cat to === tac // false 
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
